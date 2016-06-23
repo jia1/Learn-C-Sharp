17e307f69defb047646055194f72d58700bc3151
Jia Yee <jiayeerawr@gmail.com>
2016-06-23 10:17:48 +0800

    Add Hello, World and int.Parse

--- Learn-C-Sharp/Program.cs
+++ Learn-C-Sharp/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿// using is a keyword which imports a namespace (a collection of classes)
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Learn_C_Sharp
+namespace LearnCSharp
 {
     class Program
     {
+        // bool, int, string, char, float
+        // <visibility> <data type> <name> = <value>;
         static void Main(string[] args)
         {
+            int number;
+            Console.WriteLine("Please enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have entered " + number);
+            Console.WriteLine("Hello, World!");
+            // call ReadLine() so that the console will not close immediately after printing
+            Console.ReadLine();
         }
     }
 }
