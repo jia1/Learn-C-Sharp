dce920d6f23eaea8e57d73bc13ed8ca671cffacd
Jia Yee <jiayeerawr@gmail.com>
2016-06-23 10:22:28 +0800

    Add switch case

--- Learn-C-Sharp/Program.cs
+++ Learn-C-Sharp/Program.cs
@@ -17,6 +17,16 @@ namespace LearnCSharp
             Console.WriteLine("Please enter a number: ");
             number = int.Parse(Console.ReadLine());
             Console.WriteLine("You have entered " + number);
+            switch(number)
+            {
+                case 0:
+                case 1:
+                    Console.WriteLine("Which is 0 or 1");
+                    break;
+                default:
+                    Console.WriteLine("Which is some other number that is neither 0 nor 1");
+                    break;
+            }
             Console.WriteLine("Hello, World!");
             // call ReadLine() so that the console will not close immediately after printing
             Console.ReadLine();
